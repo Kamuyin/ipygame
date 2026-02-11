8fc159fefccaf864084e76b917706f9580eb966a
Kamuyin <56199686+Kamuyin@users.noreply.github.com>
2026-02-11 17:46:55 +0100

    fix(docs): set base url

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kamuyin.github.io',
+	base: '/ipygame/',
 	integrations: [
 		starlight({
 			title: 'ipygame',
